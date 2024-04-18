d015436668fa5b0dcc585a714da3f380ccf78ee5
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-04-18 14:13:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# roomkit-web-vue
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/KaustubhKumar05/roomkit-web-vue)
---
+++ src/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div id="form" v-if="showInputForm">
+    <p>Join a 100ms room</p>
+    <input
+      id="room-code-field"
+      v-model="roomCode"
+      type="text"
+      placeholder="Enter room code"
+    />
+    <button id="btn" @click="renderHmsPrebuilt">Submit</button>
+  </div>
+  <div id="app">
+    <hms-prebuilt
+      v-if="showHmsPrebuilt"
+      :room-code="roomCode"
+      @on-leave="log"
+    ></hms-prebuilt>
+  </div>
+</template>
+
+<script>
+import '@100mslive/roomkit-web';
+
+window.log = () => console.log('on-leave');
+
+export default {
+  data() {
+    return {
+      roomCode: '',
+      showHmsPrebuilt: false,
+      showInputForm: true,
+    };
+  },
+  methods: {
+    renderHmsPrebuilt() {
+      this.showHmsPrebuilt = true;
+      this.showInputForm = false;
+    },
+    resetPage() {
+      this.showHmsPrebuilt = false;
+      this.showInputForm = true;
+    },
+    log() {
+      console.log('before on-leave');
+      window.log();
+      console.log('after on-leave');
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+}
+
+#form {
+  background-color: black;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  color: white;
+}
+
+#btn {
+  padding: 0.5rem;
+  background-color: #2572ed;
+  color: white;
+  border-radius: 0.25rem;
+  width: 10rem;
+  border: none;
+}
+
+#room-code-field {
+  padding: 0.5rem;
+  border-radius: 0.25rem;
+  border: 2px solid #2572ed;
+  background: none;
+  color: white;
+  text-align: center;
+}
+
+#app {
+  height: 100vh;
+}
+</style>
